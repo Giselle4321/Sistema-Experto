a97e0315a3e91564bb9fed1812cebe5bad3dafd2
Giselle4321 <21al7207486@unedl.mx>
2024-07-30 21:03:07 -0600

    Add files via upload

---
+++ anime/motor_de_inferencia_anime.py
@@ -0,0 +1,18 @@
+# motor_de_inferencia.py
+
+# Importa la función unidecode de la biblioteca unidecode
+from unidecode import unidecode
+from base_de_conocimientos_anime import animes
+
+def recomendar_mejores_animes_por_genero(genero, top_n=5):
+    # Normalizar el género
+    genero_normalizado = unidecode(genero).lower()
+    
+    # Filtrar animes por género
+    animes_del_genero = [anime for anime in animes if unidecode(anime['genero']).lower() == genero_normalizado]
+    
+    # Ordenar animes por calificación de mayor a menor
+    animes_del_genero_ordenados = sorted(animes_del_genero, key=lambda x: x['calificacion'], reverse=True)
+    
+    # Devolver los top_n animes
+    return animes_del_genero_ordenados[:top_n]
